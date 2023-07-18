eeb4e449f64e5c81236a5b0c7e002f16a5c0d764
otvertka <otvertka123@ukr.net>
2023-07-18 13:58:54 +0300

    add toggle

--- src/App.js
+++ src/App.js
@@ -6,20 +6,42 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   const toggleParagraph = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow toggle !</Button>
       <Button onClick={toggleParagraph}>Toggle paragraph !</Button>
     </div>
   );
 }
 
 export default App;
+
+
+function logPerson() {
+  console.log(`Person: ${this.name}, ${this.age}, ${this.job}`);
+}
+
+const person1 = {name: 'Miho', age: 22, 'Frontend'};
+const person2 = {name: 'Elena', age: 32, 'SMM'};
+
+const bind = function(pers, log) {
+  return log() 
+}
+
+bind(person1, logPerson) 
